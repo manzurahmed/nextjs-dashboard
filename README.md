a63a58b9eac0658619929d24099c60bb43715320
Manzur Ahmed <monjur.ahmed@gmail.com>
2024-12-20 22:49:35 +0600

    Chapter 15 completed
    Chapter 15 completed

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Vercel: [https://nextjs-dashboard-six-tau-86.vercel.app/]
 
 ## Progress
 
-[https://nextjs.org/learn/dashboard-app/mutating-data]
+[https://nextjs.org/learn/dashboard-app/adding-authentication]
 
 ## Packages
 
@@ -14,7 +14,30 @@ pnpm i @vercel/postgres
 pnpm i next@canary
 // Debouncing
 pnpm i use-debounce
+// 
 ```
+
+## ESLint
+
+Optionally, if you would like to try this out, add next lint as a script in your package.json file:
+
+```bash
+"scripts": {
+    "build": "next build",
+    "dev": "next dev",
+    "start": "next start",
+    "lint": "next lint"
+},
+```
+Then run pnpm lint in your terminal:
+
+```bash
+pnpm lint
+```
+
+## Neon Database
+
+Log in with GitHub account.
 
 ## Next.js App Router Course - Starter
 
---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,40 @@
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+// C13
+import { notFound } from 'next/navigation';
+
+export default async function Page(
+	props: { params: Promise<{ id: string }> }
+) {
+	const params = await props.params;
+	const id = params.id;
+
+	const [invoice, customers] = await Promise.all([
+		fetchInvoiceById(id),
+		fetchCustomers(),
+	]);
+
+	// C13
+	// That's something to keep in mind,
+	// notFound will take precedence over error.tsx
+	if (!invoice) {
+		notFound();
+	}
+
+	return (
+		<main>
+			<Breadcrumbs
+				breadcrumbs={[
+					{ label: 'Invoices', href: '/dashboard/invoices' },
+					{
+						label: 'Edit Invoice',
+						href: `/dashboard/invoices/${id}/edit`,
+						active: true,
+					},
+				]}
+			/>
+			<Form invoice={invoice} customers={customers} />
+		</main>
+	);
+}
